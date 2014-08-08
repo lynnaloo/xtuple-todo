c13d0873589b4ff2b1eacec6df6aee89bf7dfde7
Linda Nichols <lynnaloo@gmail.com>
2014-08-08 17:38:51 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,75 @@
+var express = require('express'),
+  app = express(),
+  path = require('path'),
+  http = require('http'),
+  bodyParser = require('body-parser'),
+  Client = require('xtuple-rest-client');
+
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.use(bodyParser());
+app.locals.title = "xTuple REST To Do App";
+app.set("views", path.join( __dirname, "views"));
+
+app.get('/', function (req, res){
+  new Client(function (client) {
+    client.query({
+      type: 'ToDo',
+      method: 'list',
+      params: { maxResults: 50 },
+      callback: function (err, result) {
+        if (err) {
+          res.send('Error:', err);
+        }
+        if (result) {
+          app.locals.todos = result.data.data;
+          res.render("index");
+        }
+      }
+    });
+  });
+});
+
+app.get("/add_todo", function (req, res){
+  new Client(function (client) {
+    client.query({
+      type: 'ToDo',
+      method: 'insert',
+      params: {
+        dueDate: req.query.duedate,
+        status: req.query.status,
+        isActive: true,
+        description: req.query.description
+      },
+      callback: function (err, result) {
+        if (err) {
+          res.send('Error:', err);
+          return;
+        }
+        justAdded = result.data.id;
+        res.send('Inserted:', result.data.id);
+      }
+    });
+  });
+});
+
+app.get("/get_todo", function (req, res){
+  new Client(function (client) {
+    client.query({
+      type: 'ToDo',
+      method: 'get',
+      params: {uuid: req.query.id},
+      callback: function (err, result) {
+        if (err) {
+          res.send('Error:', err);
+          return;
+        }
+        res.send('To Do:', result);
+      }
+    });
+  });
+});
+
+http.createServer(app).listen(app.get("port"), function () {
+  console.log("xTuple REST To Do app is running at localhost:", app.get("port"));
+});
